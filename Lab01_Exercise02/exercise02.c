1dfcf9c720c249259f7e6c70269a749f977a71fa
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2020-02-13 09:44:10 +0000

    Fix for Lab1, Ex2.6
    Use of signed int was truncating 64 bit value.
    
    Have emailed tweak to lab sheet to Paul.

--- Lab01_Exercise02/exercise02.c
+++ Lab01_Exercise02/exercise02.c
@@ -11,8 +11,8 @@ int main()
 
 	unsigned long long int sum = 0;		//sum is a an unsigned long long int as it adds only positive integer values
 	unsigned int average = 0;			//average is unsigned as all random numbers are positive. It does not need to be 64bit as the value can not exceed the range.
-	signed int min = 0;					//can be signed as we normalise so the range can not be exceeded
-	signed int max = 0;					//can be signed as we normalise so the range can not be exceeded
+	signed long long min = 0;			//can be signed as we normalise so the range can not be exceeded
+	signed long long max = 0;			//can be signed as we normalise so the range can not be exceeded
 
 	unsigned char i = 0;				//unsigned to ensure that we can hold the full range of 0-255
 
@@ -25,17 +25,17 @@ int main()
 		//printf("value = %u, sum = %llu\n", values[i], sum);
 	}
 
-	average = (unsigned int) (sum / NUM_VALUES);	//once sum is divded by NUM_VALUES ist will be in 32 bit unsigned range (it can not be larger than the largest unsigned int returned from random_uint())
+	average = (unsigned int) (sum / NUM_VALUES);	//once sum is divided by NUM_VALUES ist will be in 32 bit unsigned range (it can not be larger than the largest unsigned int returned from random_uint())
 
 	for (i=0; i<NUM_VALUES; i++){
 		values[i] -= average;										//values is signed so it can hold the normalised values
-		min = (values[i] < min)? (signed int) values[i] : min;		//needs a cast to signed int (values[i] will not exceed the range as it has been normalised so no longer required the full positive range of an unsigned int)
-		max = (values[i] > max) ? (signed int) values[i] : max;		//needs a cast to signed int (values[i] will not exceed the range as it has been normalised so no longer required the full positive range of an unsigned int)
+		min = (values[i] < min) ? values[i] : min;
+		max = (values[i] > max) ? values[i] : max;
 	}
 
 	printf ("Sum is \t%llu\n", sum);
 	printf ("Average is %u\n", average);
-	printf ("Min is %d\n", min);
-	printf ("Max is %u\n", max);
+	printf ("Min is %lld\n", min);
+	printf ("Max is %lld\n", max);
     return 0;
 }
